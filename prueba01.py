ec79ec766abe79636f9e140c6075ad717b618bd6
Matias Guarnera <matiasguarnera@gmail.com>
2023-05-14 21:42:36 -0300

    Separa las filas que quiero hasta que haya una en blanco. Un avance

--- prueba01.py
+++ prueba01.py
@@ -1,10 +1,18 @@
 import openpyxl
 
-wb = openpyxl.load_workbook("bajada_sap.xlsx")
-print(type(wb))
-sheets = wb.sheetnames
-print(sheets)
+wb = openpyxl.load_workbook("bajada_lastres.xlsx")
+# print(type(wb))
 
-wb.active.title = "Boca"
+ppal = wb.active
+wb.create_sheet("Certificado")
+ppal.title = "Boca"
+for x in range(6, 1000):
+    if ppal.cell(row=x, column=3).value != None:
+        wb["Certificado"].cell(row=x, column=1).value = (
+            wb["Boca"].cell(row=x, column=3).value
+        )
+    else:
+        break
+
 
 wb.save("bajada_sap2.xlsx")
